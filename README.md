89188aa35df551c753c27f3f75ae28db4f44e34c
trewanek <trewanek@gmail.com>
2019-03-31 22:40:51 +0900

    add source files

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # verification_mercari_datastore_boom
+
+## 個人的な検証用PJ
+
+下記検証ライブラリ。
+書き方の差異による性能検証やそもそもの使い方、書き方の検証が目的。
+
+- [mercari/datastore](https://github.com/mercari/datastore)
+- [boom](https://github.com/mercari/datastore/tree/master/boom) 
+
+## PJ情報
+
+- Go1.9.7
+- パッケージ管理([dep](https://github.com/golang/dep))
+
+## Datastore認証
+
+下記のようにGCPのサービスアカウントの認証鍵ファイルでDatastoreClientを作成している。
+
+```go
+func CreateBoom(ctx context.Context) *boom.Boom {
+	credentialPath := os.Getenv("CREDENTIAL_FILE_PATH")
+	opts := datastore.WithCredentialsFile(credentialPath)
+	cli, _ := clouddatastore.FromContext(ctx, opts)
+	return boom.FromClient(ctx, cli)
+}
+```
+
+下記手順を実施することで実行可能。
+
+- Datastore書き込み権限のあるサービスアカウントを作成
+- サービスアカウントの鍵ファイルをDL
+- 鍵ファイルのPATHを`CREDENTIAL_FILE_PATH`の環境変数に設定
+
---
+++ main.go
@@ -0,0 +1,332 @@
+package main
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"go.mercari.io/datastore"
+	"go.mercari.io/datastore/boom"
+	"go.mercari.io/datastore/clouddatastore"
+	"golang.org/x/sync/errgroup"
+	"log"
+	"os"
+	"reflect"
+	"runtime"
+	"sync"
+	"time"
+)
+
+type DummyObject struct {
+	ID   string `datastore:"-" boom:"id"`
+	Parent datastore.Key
+	Name string
+}
+
+type DummyObject2 struct {
+	ID   string `datastore:"-" boom:"id"`
+	Name string
+}
+
+type DummyObject3 struct {
+	ID   string `datastore:"-" boom:"id"`
+	Name string
+}
+
+func main() {
+	var err error
+	ctx := context.Background()
+	b := CreateBoom(ctx)
+
+	err = BenchFuncSeconds(PutMultiWithTxBoom, b)
+	if err != nil {
+		log.Printf("PutMultiWithTxBoom err: %v", err)
+	}
+
+	err = BenchFuncSeconds(PutMultiWithTxBoom2, b)
+	if err != nil {
+		log.Printf("PutMultiWithTxBoom err: %v", err)
+	}
+
+	err = BenchFuncSeconds(PutMultiWithTx, b)
+	if err != nil {
+		log.Printf("PutMultiWithTx err: %v", err)
+	}
+
+	err = BenchFuncSeconds(PutMultiWithWaitGroup, b)
+	if err != nil {
+		log.Printf("PutMultiWithWaitGroup err: %v", err)
+	}
+
+	err = BenchFuncSeconds(PutMultiWithErrorGroupTx, b)
+	if err != nil {
+		log.Printf("PutMultiWithErrorGroupTx err: %v", err)
+	}
+
+	err = BenchFuncSeconds(PutMultiWithErrorGroupBoom, b)
+	if err != nil {
+		log.Printf("PutMultiWithErrorGroupBoom err: %v", err)
+	}
+
+	err = BenchFuncSeconds(CreateDatastoreKeies, b)
+	if err != nil {
+		log.Printf("CreateDatastoreKeies err: %v", err)
+	}
+}
+
+func GetDoList() []*DummyObject {
+	return []*DummyObject{
+		{ID: uuid.New().String(), Name: "Hoge"},
+		{ID: uuid.New().String(), Name: "Hoge"},
+		{ID: uuid.New().String(), Name: "Hoge"},
+		{ID: uuid.New().String(), Name: "Hoge"},
+		{ID: uuid.New().String(), Name: "Hoge"},
+	}
+}
+
+func GetDoList2() []*DummyObject2 {
+	return []*DummyObject2{
+		{ID: uuid.New().String(), Name: "Hoge"},
+		{ID: uuid.New().String(), Name: "Hoge"},
+		{ID: uuid.New().String(), Name: "Hoge"},
+		{ID: uuid.New().String(), Name: "Hoge"},
+		{ID: uuid.New().String(), Name: "Hoge"},
+	}
+}
+
+func GetDoList3() []*DummyObject3 {
+	return []*DummyObject3{
+		{ID: uuid.New().String(), Name: "Hoge"},
+		{ID: uuid.New().String(), Name: "Hoge"},
+		{ID: uuid.New().String(), Name: "Hoge"},
+		{ID: uuid.New().String(), Name: "Hoge"},
+		{ID: uuid.New().String(), Name: "Hoge"},
+	}
+}
+
+func CreateBoom(ctx context.Context) *boom.Boom {
+	credentialPath := os.Getenv("CREDENTIAL_FILE_PATH")
+	opts := datastore.WithCredentialsFile(credentialPath)
+	cli, _ := clouddatastore.FromContext(ctx, opts)
+	return boom.FromClient(ctx, cli)
+}
+
+func PutMultiWithTxBoom(b *boom.Boom) error {
+	if _, err := b.RunInTransaction(func(tx *boom.Transaction) error {
+		doList := GetDoList()
+		_, err := tx.Boom().PutMulti(doList)
+		if err != nil {
+			return err
+		}
+		doList2 := GetDoList2()
+		_, err = tx.Boom().PutMulti(doList2)
+		if err != nil {
+			return err
+		}
+		doList3 := GetDoList3()
+		_, err = tx.Boom().PutMulti(doList3)
+		if err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func PutMultiWithTxBoom2(b *boom.Boom) error {
+	if _, err := b.RunInTransaction(func(tx *boom.Transaction) error {
+		bm := tx.Boom()
+		doList := GetDoList()
+		_, err := bm.PutMulti(doList)
+		if err != nil {
+			return err
+		}
+		doList2 := GetDoList2()
+		_, err = bm.PutMulti(doList2)
+		if err != nil {
+			return err
+		}
+		doList3 := GetDoList3()
+		_, err = bm.PutMulti(doList3)
+		if err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		return err
+	}
+	return nil
+}
+
+func PutMultiWithTx(b *boom.Boom) error {
+	_, err := b.RunInTransaction(func(tx *boom.Transaction) error {
+		doList := GetDoList()
+		_, err := tx.PutMulti(doList)
+		if err != nil {
+			return err
+		}
+		doList2 := GetDoList2()
+		_, err = tx.PutMulti(doList2)
+		if err != nil {
+			return err
+		}
+		doList3 := GetDoList3()
+		_, err = tx.PutMulti(doList3)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func PutMultiWithWaitGroup(b *boom.Boom) error {
+	_, _ = b.RunInTransaction(func(tx *boom.Transaction) error {
+		funcs := []func(){
+			func() {
+				doList := GetDoList()
+				tx.PutMulti(doList)
+			},
+			func() {
+				doList2 := GetDoList2()
+				tx.PutMulti(doList2)
+			},
+			func() {
+				doList3 := GetDoList3()
+				tx.PutMulti(doList3)
+			},
+		}
+
+		var waitGroup sync.WaitGroup
+		for _, f := range funcs {
+			waitGroup.Add(1)
+			go func(function func()) {
+				defer waitGroup.Done()
+				function()
+			}(f)
+		}
+		waitGroup.Wait()
+		return nil
+	})
+	return nil
+}
+
+func PutMultiWithErrorGroupTx(b *boom.Boom) error {
+	_, err := b.RunInTransaction(func(tx *boom.Transaction) error {
+		funcs := []func() error {
+			func() error {
+				doList := GetDoList()
+				_, err := tx.PutMulti(doList)
+				if err != nil {
+					return err
+				}
+				return nil
+			},
+			func() error {
+				doList2 := GetDoList2()
+				_, err := tx.PutMulti(doList2)
+				if err != nil {
+					return err
+				}
+				return nil
+			},
+			func() error {
+				doList3 := GetDoList3()
+				_, err := tx.PutMulti(doList3)
+				if err != nil {
+					return err
+				}
+				return nil
+			},
+		}
+		eg := errgroup.Group{}
+		for _, f := range funcs {
+			eg.Go(func() error {
+				err := f()
+				if err != nil {
+					return err
+				}
+				return nil
+			})
+		}
+		if err := eg.Wait(); err != nil {
+			return err
+		}
+		return nil
+	})
+	return err
+}
+
+func PutMultiWithErrorGroupBoom(b *boom.Boom) error {
+	_, err := b.RunInTransaction(func(tx *boom.Transaction) error {
+		bm := tx.Boom()
+		funcs := []func() error {
+			func() error {
+				doList := GetDoList()
+				_, err := bm.PutMulti(doList)
+				if err != nil {
+					return err
+				}
+				return nil
+			},
+			func() error {
+				doList2 := GetDoList2()
+				_, err := bm.PutMulti(doList2)
+				if err != nil {
+					return err
+				}
+				return nil
+			},
+			func() error {
+				doList3 := GetDoList3()
+				_, err := bm.PutMulti(doList3)
+				if err != nil {
+					return err
+				}
+				return nil
+			},
+		}
+		eg := errgroup.Group{}
+		for _, f := range funcs {
+			eg.Go(func() error {
+				err := f()
+				if err != nil {
+					return err
+				}
+				return nil
+			})
+		}
+		if err := eg.Wait(); err != nil {
+			return err
+		}
+		return nil
+	})
+	return err
+}
+
+func CreateDatastoreKeies(b *boom.Boom) error {
+	_, _ = b.RunInTransaction(func(tx *boom.Transaction) error {
+		for i:=0;i<100;i++ {
+			parent := tx.Key(&DummyObject2{ID: uuid.New().String()})
+			_ = tx.Key(&DummyObject{ID: uuid.New().String(), Parent: parent})
+		}
+		return nil
+	})
+	return nil
+}
+
+func BenchFuncSeconds(f func(b *boom.Boom) error, b *boom.Boom) error {
+	st := time.Now()
+	err := f(b)
+	ed := time.Now()
+	fv := reflect.ValueOf(f)
+	funcName := runtime.FuncForPC(fv.Pointer()).Name()
+	if err != nil {
+		log.Printf("%v - %s - faild. err: %v", ed.Sub(st), funcName, err)
+	}
+	log.Printf("%v - %s", ed.Sub(st), funcName)
+	return nil
+}
